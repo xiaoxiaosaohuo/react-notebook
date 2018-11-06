690b374b0a8a1e691e12ca644b355348f768f6fa
siven.jin <siven.jin@qunar.com>
2018-11-06 14:39:26 +0800

    修改react版本

--- index.js
+++ index.js
@@ -56,7 +56,10 @@ class ExampleApplication extends React.Component {
 }
 debugger;
 ReactDOM.render(
-    <h1>这是什么鬼啊</h1>,
+    <div className="contaier" style={{backgroundColor:"red"}}>
+        <h1 style={{color:"blue"}}>这是什么鬼啊</h1>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/600px-Google_%22G%22_Logo.svg.png"/>
+    </div>,
     mountNode,
     function() {}
 );
