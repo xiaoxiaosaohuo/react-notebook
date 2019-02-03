81134ea5efc3bacbf59b554e02ed419fc99f1b19
jinxin479 <jinxin479@126.com>
2019-02-03 19:53:32 +0800

    D

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
-const Koa = require('koa');
+const Koa = require("koa");
 const app = new Koa();
 
 app.use(async ctx => {
-  ctx.body = 'Hello World';
+  ctx.body = "Hello World";
 });
 
-app.listen(3000);
+app.listen(80);
