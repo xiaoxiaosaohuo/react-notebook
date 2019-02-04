ca84a1ae084f8b6b2939d1507c6d5700a307536a
jinxin479 <jinxin479@126.com>
2019-02-04 23:06:26 +0800

    d

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,5 +10,17 @@ module.exports = {
         NODE_ENV: "production"
       }
     }
-  ]
+  ],
+  deploy: {
+    production: {
+      key: ~/.ssh/,
+      user: "root",
+      host: "104.155.232.100",
+      ref: "origin/master",
+      repo: "git@github.com:jinxin479/react-notebook.git",
+      path: "/home/jinxin479_gmail_com/home/www",
+      "pre-deploy-local": "echo 'Deploy Done'",
+      "post-deploy": "npm install"
+    }
+  }
 };
