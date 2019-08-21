58067b3ae47b8d481c16041fc3a95c1cad96db5d
jinxin479 <jinxin479@126.com>
2019-08-21 10:18:12 +0800

    e
    e

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @Description: In User Settings Edit
  * @Author: your name
  * @Date: 2019-08-08 13:16:57
- * @LastEditTime: 2019-08-19 19:42:18
+ * @LastEditTime: 2019-08-19 20:22:00
  * @LastEditors: Please set LastEditors
  */
 import ReactRedux from "./src/demo";
@@ -16,18 +16,31 @@ const mountNode = document.getElementById("root");
 //   }
 // }).install();
 // import SelectTickets from './src/counter';
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state={
         a:1
     }
+    
   }
   changeState= ()=>{
     
   }
   onClick = () =>{
-    
+    const data = [
+      [1,2,3,4,6,7,8],
+      [11,22,13,24,16,17,48],
+      [31,32,43,54,66,77,48],
+    ]
+    const result = data.map(item=>{
+      return item.map(data=>{
+        return data % 2 ===0 ? '偶数' :'奇数'
+      })
+    })
+
+    console.log(result)
 
   }
   componentDidMount(){
